1e2db3bf0b1b2f0626c8ee9beed22581264aacb1
CheolYong <cjfdyd4257@gmail.com>
2021-09-20 13:02:08 +0900

    문자열 내림차순으로 배치하기

---
+++ Lv1/문자열 내림차순으로 배치하기.cs
@@ -0,0 +1,14 @@
+using System;
+public class Solution {
+    public string solution(string s) {
+        string answer = "";
+
+            char[] arr = s.ToCharArray();
+
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+            answer = new string(arr);
+        return answer;
+    }
+}
