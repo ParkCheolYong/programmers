b866103bcf44cee6b23c8aa316a34908a7c9e56c
CheolYong <cjfdyd4257@gmail.com>
2021-09-30 23:08:53 +0900

    자연수 뒤집어 배열로 만들기

---
+++ Lv1/자연수 뒤집어 배열로 만들기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int[] solution(long n) {
+        string s = n.ToString();
+        int[] answer = new int[s.Length] ;
+        
+        
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                answer[s.Length - i - 1] = int.Parse(s[i].ToString());
+            }
+        
+        return answer;
+    }
+}
