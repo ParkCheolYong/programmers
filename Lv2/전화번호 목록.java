ca52ff8c0db88d33aba5b625c132b482819d2ec1
CheolYong <cjfdyd4257@gmail.com>
2022-02-03 18:29:40 +0900

    전화번호 목록

---
+++ Lv2/전화번호 목록.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;        
+
+        Map<String, String> map = new HashMap<String, String>();
+
+       for(String s : phone_book)
+       {
+           map.put(s, s);
+       }
+
+       for (int i = 0; i < phone_book.length; i++)
+       {
+           for (int j = 1; j < phone_book[i].length(); j++)
+           {
+               if (map.containsKey(phone_book[i].substring(0,j)))
+               {
+                   return false;
+               }
+           }
+       }  
+
+        return answer;
+    }
+}
