6463e32509f5534cc1c486754d95085d64cbbf25
CheolYong <cjfdyd4257@gmail.com>
2023-02-03 17:42:44 +0900

    대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기

---
+++ Lv3/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -0,0 +1,18 @@
+SELECT MONTH(START_DATE) AS MONTH
+     , CAR_ID
+     , COUNT(*)          AS RECORDS
+  FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+ WHERE CAR_ID IN (  
+                  SELECT  CAR_ID
+                    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                   WHERE START_DATE BETWEEN '20220801' AND '20221031'
+                   GROUP BY CAR_ID    
+                   HAVING COUNT(*) >= 5		
+				 )
+   AND START_DATE BETWEEN '20220801' AND '20221031'                 
+ GROUP BY 
+       MONTH(START_DATE)
+     , CAR_ID    
+ ORDER BY 
+       MONTH
+     , CAR_ID DESC;
