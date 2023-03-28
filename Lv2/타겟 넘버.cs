87a79401856f240e3942cf4cc4e13ad2fcb77875
ParkCheolYong <51697316+ParkCheolYong@users.noreply.github.com>
2023-03-28 12:26:09 +0900

    Update 타겟 넘버.cs

--- Lv2/타겟 넘버.cs
+++ Lv2/타겟 넘버.cs
@@ -1,37 +1,22 @@
 using System;
 
-public class Solution {
-    private int ANSWER = 0;
-
-    public int solution(int[] numbers, int target) {
-        //int answer = 0;
-
-        dfs(target, numbers, 0);
-
-        return ANSWER;
+public class Solution 
+{
+    public int solution(int[] numbers, int target) 
+    {
+        return DFS(numbers, target, 0, 0);
     }
-
-    private void dfs(int target, int[] numbers, int k)
-        {
-            if (k == numbers.Length)
-            {
-                int sum = 0;
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    sum += numbers[i];
-                }
-
-                if (sum == target)
-                {
-                    ANSWER++;
-                }
-            }
-            else
-            {
-                dfs(target, numbers, k + 1);
-
-                numbers[k] *= -1;
-                dfs(target, numbers, k + 1);
-            }
+    
+    public int DFS(int[] numbers, int target, int i, int sum)
+    {
+        if (i == numbers.Length)
+        {                        
+            if (sum == target) return 1;
+            else return 0;
         }
+        else
+        {
+            return DFS(numbers, target, i + 1, sum + numbers[i]) + DFS(numbers, target, i + 1, sum - numbers[i]);                
+        }        
+    }
 }
